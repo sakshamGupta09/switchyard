42dc3067248e8134dbdac3dc3c2e6dd1f39e2fff
Saksham Gupta <saksham.09.gupta@gmail.com>
2026-09-22 18:09:17 +0530

    Add MongoDB driver and initialize client.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-	"log"
+	"context"
 	"log/slog"
+	"os"
 	"switchyard/internal/config"
+	"switchyard/internal/database/mongo"
 	"switchyard/internal/logger"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Logger setup
+	appLogger := logger.New()
+	slog.SetDefault(appLogger)
+
+	// Environment variables setup
 	godotenv.Load()
 	config, err := config.Load()
 
 	if err != nil {
-		log.Fatal("Failed to load required configurations", err)
+		slog.Error("Failed to load required configurations", "error", err)
+		os.Exit(1)
 	}
 
-	logger := logger.New()
-	slog.SetDefault(logger)
-
 	slog.Info("Config loaded successfully")
+
+	// Database setup
+	client, err := mongo.New(&config.Mongo)
+
+	if err != nil {
+		slog.Error("failed to connect DB", "error", err)
+		os.Exit(1)
+	}
+	defer cleanup(client)
+	slog.Info("mongoDB connected!")
+}
+
+func cleanup(client *mongo.Client) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Close(ctx); err != nil {
+		slog.Error("failed to close MongoDB", "error", err)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,22 +3,46 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type AppConfig struct {
 	Environment string
 	Port        string
-	MongoDBURI  string
+	Mongo       MongoConfig
 	JWTSecret   string
 }
 
+type MongoConfig struct {
+	URI         string
+	Database    string
+	MinPoolSize uint64
+	MaxPoolSize uint64
+}
+
 func Load() (*AppConfig, error) {
+	minPoolSize, err := getUint("MONGO_MIN_POOL_SIZE")
+	if err != nil {
+		return nil, err
+	}
+
+	maxPoolSize, err := getUint("MONGO_MAX_POOL_SIZE")
+
+	if err != nil {
+		return nil, err
+	}
+
 	cfg := &AppConfig{
 		Environment: getEnv("ENV"),
 		Port:        getEnv("PORT"),
-		MongoDBURI:  getEnv("MONGODB_URI"),
 		JWTSecret:   getEnv("JWT_SECRET"),
+		Mongo: MongoConfig{
+			URI:         getEnv("MONGO_URI"),
+			Database:    getEnv("MONGO_DATABASE"),
+			MinPoolSize: minPoolSize,
+			MaxPoolSize: maxPoolSize,
+		},
 	}
 
 	if err := validate(cfg); err != nil {
@@ -35,19 +59,23 @@ func validate(cfg *AppConfig) error {
 	var missing []string
 
 	if cfg.Environment == "" {
-		missing = append(missing, "Environment")
+		missing = append(missing, "ENV")
 	}
 
 	if cfg.Port == "" {
-		missing = append(missing, "Port")
-	}
-
-	if cfg.MongoDBURI == "" {
-		missing = append(missing, "MongoDBURI")
+		missing = append(missing, "PORT")
 	}
 
 	if cfg.JWTSecret == "" {
-		missing = append(missing, "JWTSecret")
+		missing = append(missing, "JWT_SECRET")
+	}
+
+	if cfg.Mongo.URI == "" {
+		missing = append(missing, "MONGO_URI")
+	}
+
+	if cfg.Mongo.Database == "" {
+		missing = append(missing, "MONGO_DATABASE")
 	}
 
 	if len(missing) > 0 {
@@ -56,4 +84,18 @@ func validate(cfg *AppConfig) error {
 	}
 
 	return nil
+}
+
+func getUint(key string) (uint64, error) {
+	value := getEnv(key)
+
+	if value == "" {
+		return 0, fmt.Errorf("%s is required", key)
+	}
+	result, err := strconv.ParseUint(value, 10, 64)
+
+	if err != nil {
+		return 0, fmt.Errorf("invalid value for %s", key)
+	}
+	return result, nil
 }
