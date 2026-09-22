73b743b2e3928416e40e0a94c11200cf768a1565
Saksham Gupta <saksham.09.gupta@gmail.com>
2026-09-22 18:18:26 +0530

    Create mongo.go

---
+++ internal/database/mongo/mongo.go
@@ -0,0 +1,46 @@
+package mongo
+
+import (
+	"context"
+	"switchyard/internal/config"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type Client struct {
+	client *mongo.Client
+	db     *mongo.Database
+}
+
+func (c *Client) Close(ctx context.Context) error {
+	if c == nil || c.client == nil {
+		return nil
+	}
+	return c.client.Disconnect(ctx)
+}
+
+func New(cfg *config.MongoConfig) (*Client, error) {
+	clientOptions := options.Client().
+		ApplyURI(cfg.URI).
+		SetMinPoolSize(cfg.MinPoolSize).
+		SetMaxPoolSize(cfg.MaxPoolSize).
+		SetServerSelectionTimeout(5 * time.Second).
+		SetConnectTimeout(5 * time.Second)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	pingCtx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	if err := client.Ping(pingCtx, nil); err != nil {
+		client.Disconnect(context.Background())
+		return nil, err
+	}
+	return &Client{client: client, db: client.Database(cfg.Database)}, nil
+
+}
